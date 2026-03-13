ced6420413f0d0b623b37a40cb71e46e27d08681
manoj-netizenn <mallappanavarmanoj@gmail.com>
2026-03-13 22:00:28 +0530

    added variable for base Url deployment help

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin:"https://hashvault.vercel.app",
+  origin:"*",
   methods:["GET", "POST", "DELETE", "PUT"],
   credentials:true,
 }));
